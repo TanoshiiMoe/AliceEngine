055255b6602f1ce9700f164b34af5f36a99c16d8
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-06-09 22:31:14 +0900

    [feat] Update Object

--- D2D-AliceEngine/Engine/Object.cpp
+++ D2D-AliceEngine/Engine/Object.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "Object.h"
+#include "Application.h"
 
 void Object::Initialize()
 {
@@ -7,12 +8,22 @@ void Object::Initialize()
 	{
 		m_localTransform = new Transform();
 	}
+	if (m_worldTransform == nullptr)
+	{
+		m_worldTransform = new Transform();
+	}
 }
 
-void Object::LoadData()
+void Object::LoadBitmapData(const std::wstring& path)
 {
+	HRESULT hr = Application::GetInstance()->m_pD2DRenderer->CreateBitmapFromFile(
+		(Define::BASE_RESOURCE_PATH + path).c_str(), &m_bitmap);
+	assert(SUCCEEDED(hr));
 }
 
 void Object::Release()
 {
+	m_localTransform = nullptr;
+	m_worldTransform = nullptr;
+	m_bitmap = nullptr;
 }
--- D2D-AliceEngine/Engine/Object.h
+++ D2D-AliceEngine/Engine/Object.h
@@ -1,27 +1,65 @@
 #pragma once
 #include "pch.h"
 #include "Transform.h"
-#include "BitmapImage.h"
+
+using namespace Microsoft::WRL;
 
 class Object
 {
 public:
-	Object() 
+	Object()
 	{
 		m_localTransform = new Transform();
+		m_worldTransform = new Transform();
 	}
-	virtual ~Object() 
+	virtual ~Object()
 	{
-		m_localTransform = nullptr;
+		delete m_localTransform;
+		delete m_worldTransform;
+		m_bitmap = nullptr;
 	}
 
-	virtual void Initialize();
-	virtual void LoadData();
-	virtual void Release();
+	void Initialize();
+	void LoadBitmapData(const std::wstring& path);
+	void Release();
 
-	DoubleLinkedList<Transform*> childList;
+	// 계층구조 관리
+	Object* parent = nullptr;
+	std::vector<Object*> children;
+
+	void AddChild(Object* child)
+	{
+		child->parent = this;
+		children.push_back(child);
+	}
+
+	// 재귀로 월드 변환 계산
+	void UpdateWorldTransform()
+	{
+		D2D1::Matrix3x2F mat;
+
+		if (parent)
+		{
+			mat = m_localTransform->ToMatrix() * parent->m_worldTransform->ToMatrix();
+		}
+		else
+		{
+			mat = m_localTransform->ToMatrix();
+		}
+
+		m_worldTransform->SetFromMatrix(mat);
+
+		for (auto child : children)
+			child->UpdateWorldTransform();
+	}
+
+	virtual void Render()
+	{
+		// m_worldTransform 기준 렌더링
+	}
+
 	Transform* m_localTransform; // Transform 컴포넌트
 	Transform* m_worldTransform; // Transform 컴포넌트
-	BitmapImage m_bitmapImage; // BitmapImage 컴포넌트
+	ComPtr<ID2D1Bitmap1> m_bitmap; // BitmapImage 컴포넌트
 };
 
