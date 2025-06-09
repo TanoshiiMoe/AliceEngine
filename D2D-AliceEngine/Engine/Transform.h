3fc5bfb9deecd3ad5e3c81a612597cb6bc9a9ba2
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-06-09 22:31:42 +0900

    [feat] Transform Matrix for local pivot

--- D2D-AliceEngine/Engine/Transform.h
+++ D2D-AliceEngine/Engine/Transform.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "pch.h"
+
 class Transform
 {
 public:
@@ -45,10 +47,33 @@ public:
 
 	D2D1::Matrix3x2F ToMatrix() const
 	{
-		D2D1::Matrix3x2F scale = D2D1::Matrix3x2F::Scale(Scale.x, Scale.y, D2D1::Point2F(Translation.x, Translation.y));
+		D2D1::Matrix3x2F scale = D2D1::Matrix3x2F::Scale(Scale.x, Scale.y);
 		D2D1::Matrix3x2F rotation = D2D1::Matrix3x2F::Rotation(Rotation);
 		D2D1::Matrix3x2F translation = D2D1::Matrix3x2F::Translation(Translation.x, Translation.y);
 		return scale * rotation * translation;
+	}
+
+	D2D1::Matrix3x2F ToMatrix(const D2D1_POINT_2F& pivot) const
+	{
+		auto scale = D2D1::Matrix3x2F::Scale(Scale.x, Scale.y, pivot);
+		auto rotate = D2D1::Matrix3x2F::Rotation(Rotation, pivot);
+		auto translate = D2D1::Matrix3x2F::Translation(Translation.x, Translation.y);
+		return scale * rotate * translate;
+	}
+
+	void SetFromMatrix(const D2D1::Matrix3x2F& mat)
+	{
+		// 위치
+		Translation.x = mat.dx;
+		Translation.y = mat.dy;
+
+		// 스케일
+		Scale.x = std::sqrt(mat.m11 * mat.m11 + mat.m12 * mat.m12);
+		Scale.y = std::sqrt(mat.m21 * mat.m21 + mat.m22 * mat.m22);
+
+		// 회전 (라디안 → 도)
+		float radians = std::atan2(mat.m12, mat.m11);
+		Rotation = radians * (180.0f / Define::PI);
 	}
 };
 
