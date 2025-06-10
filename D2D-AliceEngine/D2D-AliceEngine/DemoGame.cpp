435547792147f8678a58133f9f7f58093be6a1e2
K-2301160047\User <ckdwls525@gmail.com>
2025-06-10 10:15:45 +0900

    [feat] add images

--- D2D-AliceEngine/D2D-AliceEngine/DemoGame.cpp
+++ D2D-AliceEngine/D2D-AliceEngine/DemoGame.cpp
@@ -17,20 +17,20 @@ void DemoGame::Initialize()
 	m_pD2DRenderer->Initialize(m_hwnd);
 
 	m_Sun = new Object();
-	m_Sun->LoadBitmapData(L"Mushroom.png");
+	m_Sun->LoadBitmapData(L"Sun.png");
 	m_Sun->m_localTransform->SetPosition(250, 150);
 	m_pD2DRenderer->m_renderList.push_back(m_Sun);
 
 	// Áö±¸
 	m_Earth = new Object();
-	m_Earth->LoadBitmapData(L"Mushroom.png");
+	m_Earth->LoadBitmapData(L"Earth.png");
 	m_Earth->m_localTransform->SetPosition(100, 0);
 	m_Sun->AddChild(m_Earth);
 	m_pD2DRenderer->m_renderList.push_back(m_Earth);
 
 	// ´Þ
 	m_Moon = new Object();
-	m_Moon->LoadBitmapData(L"Mushroom.png");
+	m_Moon->LoadBitmapData(L"Moon.png");
 	m_Moon->m_localTransform->SetPosition(50, 0);
 	m_Earth->AddChild(m_Moon);
 	m_pD2DRenderer->m_renderList.push_back(m_Moon);
