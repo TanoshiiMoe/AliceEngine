3b32da9b9fea64f696ee21aee1ea17ef831a2f8b
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-06-22 22:14:28 +0900

    [feat] update spine2D wrapper

---
+++ D2D-AliceEngine/Spine2D/3.8/include/spine/spine-alice.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <spine/spine.h>
+#include <spine/extension.h>
+#include <d2d1_1.h>
+#include <wrl/client.h>
+#include <vector>
+
+namespace spine {
+
+    using Microsoft::WRL::ComPtr;
+
+    class SkeletonDrawable_D2D {
+    public:
+        SkeletonDrawable_D2D(SkeletonData* skeletonData, AnimationStateData* stateData = nullptr);
+        ~SkeletonDrawable_D2D();
+
+        void Update(float deltaTime);
+        void Draw(ID2D1DeviceContext* context);
+
+        void SetPremultipliedAlpha(bool enabled) { usePremultipliedAlpha = enabled; }
+        bool GetPremultipliedAlpha() const { return usePremultipliedAlpha; }
+
+    private:
+        Skeleton* skeleton;
+        AnimationState* state;
+        AnimationStateData* stateData;
+        bool ownsStateData;
+
+        float timeScale = 1.0f;
+        bool usePremultipliedAlpha = false;
+
+        Vector<float> worldVertices;
+        Vector<float> tempUvs;
+        Vector<Color> tempColors;
+        Vector<unsigned short> quadIndices;
+        SkeletonClipping clipper;
+
+        ComPtr<ID2D1SolidColorBrush> debugBrush;
+
+        void InitializeBrush(ID2D1DeviceContext* context);
+    };
+
+} // namespace spine
