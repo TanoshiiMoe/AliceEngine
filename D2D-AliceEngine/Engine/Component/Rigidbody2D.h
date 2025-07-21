c1784334d57841e697b58f6360c341cdd52159c5
K-2301160047\User <ckdwls525@gmail.com>
2025-07-21 16:56:45 +0900

    [chore] fix rigidbody2d Comment

--- D2D-AliceEngine/Engine/Component/Rigidbody2D.h
+++ D2D-AliceEngine/Engine/Component/Rigidbody2D.h
@@ -5,6 +5,7 @@
 
 /*
 *	@brief : 물리를 시뮬레이션하는 컴포넌트입니다.
+*	@details : ERigidBodyState로 상태를 관리합니다.
 */
 class Rigidbody2D : public Component
 {
@@ -15,9 +16,9 @@ public:
 	void Initialize() override;
 	void Update(const float& deltaSeconds) override;
 	void Release() override;
-
 	bool AABBSweep(const FAABB& boxStart, const FAABB& boxEnd, const FAABB& target, float& outT, FVector2& outHitPos);
-	// 물리 속성 변수들
+
+	// <물리 속성 변수들>
 	FVector2 velocity = FVector2(0.0f, 0.0f);           // 선속도
 	float angularVelocity = 0.0f;                     // 각속도
 	float mass = 1.0f;                                // 질량
@@ -26,7 +27,6 @@ public:
 	FVector2 force = FVector2(0.0f, 0.0f);              // 누적 힘
 	float torque = 0.0f;                              // 누적 토크
 	float gravityScale = 1.0f;                        // 중력 영향 배수 (빠른 낙하)
-	//bool isGround = false;
 	Define::ERigidBodyState m_eRigidBodyState = Define::ERigidBodyState::Space; // 땅에 닿았는지 여부
 	Define::ERigidBodyType m_eRigidBodyType = Define::ERigidBodyType::Kinematic;
 
