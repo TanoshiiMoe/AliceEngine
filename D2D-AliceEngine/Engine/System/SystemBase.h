7c2e6c329419780f43b48096c86f50917e2d85a8
K-2301160047\User <ckdwls525@gmail.com>
2025-07-21 17:01:45 +0900

    [fix] systembase move h to cpp

--- D2D-AliceEngine/Engine/System/SystemBase.h
+++ D2D-AliceEngine/Engine/System/SystemBase.h
@@ -5,37 +5,13 @@
 class SystemBase
 {
 public:
-	SystemBase() {}
-	virtual ~SystemBase() {}
+	SystemBase();
+	virtual ~SystemBase();
 
 public:
-	void Regist(const WeakObjectPtr<Component>& _component)
-	{
-		if (!_component.expired())
-		{
-			m_Components.emplace_back(_component);
-		}
-	}
-
-	void UnRegist(WeakObjectPtr<Component>&& _component)
-	{
-		for (auto it = m_Components.begin(); it != m_Components.end(); ++it)
-		{
-			if ((*it).lock() && _component.lock())
-			{
-				if ((*it).lock() == _component.lock())
-				{
-					m_Components.erase(it);
-					return;
-				}
-			}
-		}
-	}
-
-	void UnRegistAll()
-	{
-		m_Components.clear();
-	}
+	void Regist(const WeakObjectPtr<Component>& _component);
+	void UnRegist(WeakObjectPtr<Component>&& _component);
+	void UnRegistAll();
 
 protected:
 	std::vector<WeakObjectPtr<Component>> m_Components;
