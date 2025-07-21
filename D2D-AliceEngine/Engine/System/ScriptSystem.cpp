8dbebf9f41f2ede74ba8ddcce354cf80cbcd87bb
K-2301160047\User <ckdwls525@gmail.com>
2025-07-21 17:02:15 +0900

    [fix] ScriptSystem move h to cpp

--- D2D-AliceEngine/Engine/System/ScriptSystem.cpp
+++ D2D-AliceEngine/Engine/System/ScriptSystem.cpp
@@ -1,2 +1,20 @@
 #include "pch.h"
 #include "ScriptSystem.h"
+
+void ScriptSystem::Enque(WeakObjectPtr<Component> InTarget, Define::EScriptGroup InGroup, std::function<void()> TickFunc)
+{
+	m_scriptQueues[InGroup].emplace(InTarget, TickFunc);
+}
+
+void ScriptSystem::ProcessScriptGroup(Define::EScriptGroup group)
+{
+	currnetGroup = group;
+	while (m_scriptQueues[group].empty() == false)
+	{
+		if (!m_scriptQueues[group].front().Target.expired())
+		{
+			m_scriptQueues[group].front().TickFunc();
+		}
+		m_scriptQueues[group].pop();
+	}
+}
