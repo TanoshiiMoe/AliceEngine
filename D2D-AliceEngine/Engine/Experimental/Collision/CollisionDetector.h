7d70e5a8fdc7265e58c933e918cc7fb21e992f21
K-2301160047\User <ckdwls525@gmail.com>
2025-07-21 16:53:02 +0900

    [chore] erase useless code

--- D2D-AliceEngine/Engine/Experimental/Collision/CollisionDetector.h
+++ D2D-AliceEngine/Engine/Experimental/Collision/CollisionDetector.h
@@ -32,48 +32,5 @@ namespace Physics
 		static bool IsOnGround(const FAABB& above, const FAABB& below, float threshold = 0.3f);
 		static void SavePreviousCollisionData(Collider* src, Collider* tar, std::unordered_set<Rigidbody2D*>& overlappedRigdBodies);
 	};
-
-	struct NodeEntry
-	{
-		UObject* obj;
-		NodeEntry* prev;
-		NodeEntry* next;
-
-		NodeEntry()
-		{
-			prev = nullptr;
-			next = nullptr;
-			obj = nullptr;
-		}
-	};
-
-	struct Rectangle
-	{
-		float x, y, length;
-	};
-
-	//class QuadTree
-	//{
-	//public:
-	//	QuadTree();
-	//	~QuadTree();
-	//
-	//	UObject* head;
-	//	UINT nodeID = -1;
-	//	//  ------------------- 
-	//	//  |        |        |
-	//	//  |    1   |   2    |
-	//	//  |--------|--------|
-	//	//  |    3   |   4    |
-	//	//  |        |        |
-	//	//  ------------------- 
-	//	QuadTree* child[2][2];
-	//	Rectangle rect;
-	//
-	//	static void MakeQuadTree(QuadTree* tree, int& nodeID, const int& level);
-	//	static void FindNode(QuadTree* tree, const int& level, UObject** Node, const FVector3& minVector, const FVector3& maxVector);
-	//	static void UpdateNode(QuadTree* tree, const int& level, UObject* object, const FVector3& minVector, const FVector3& maxVector);
-	//	static void ClearNode(QuadTree* tree, const int& level);
-	//};
 }
 
