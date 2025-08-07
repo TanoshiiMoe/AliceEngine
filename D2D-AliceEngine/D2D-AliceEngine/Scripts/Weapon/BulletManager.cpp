15e79c495a95aa4cb04e61c34313f22f6d2e00da
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-08-07 20:19:54 +0900

    [feat] add Manager Function

--- D2D-AliceEngine/D2D-AliceEngine/Scripts/Weapon/BulletManager.cpp
+++ D2D-AliceEngine/D2D-AliceEngine/Scripts/Weapon/BulletManager.cpp
@@ -1,6 +1,7 @@
 #include "BulletManager.h"
 #include <Scripts/Weapon/Bullet.h>
 #include <Manager/SceneManager.h>
+#include <Math/TMath.h>
 
 BulletManager::BulletManager()
 {
@@ -10,23 +11,27 @@ BulletManager::~BulletManager()
 {
 }
 
-void BulletManager::FireBullet(const FVector2& start, const FVector2& target)
+void BulletManager::FireBullet(const FVector2& start, const FVector2& target, const FVector2& initVelocity)
 {
 	Scene* curScene =SceneManager::GetInstance().GetWorld();
 	auto bulletObj = curScene->NewObject<gameObject>(L"bullet");
 	bulletObj->SetPosition(start);
+	bulletObj->SetScale(FVector2(0.2, 0.2));
 	auto bullet = bulletObj->AddComponent<Bullet>();
+	bullet->inheritedVelocity = initVelocity;
 
 	FVector2 dir = target - start;
 	float distance = dir.Length();
-	dir.Normalize();
-	FVector2 perp(-dir.y, dir.x); // 수직 방향
+	FVector2 dirNormal = dir.Normalize();
+	FVector2 perp(-dirNormal.y, dirNormal.x); // 수직 방향
 
+	float randMultiply = FRandom::GetRandomInRange(0.1, 0.9);
 	//컨트롤 포인트 계산: 시작점과 끝점 중간에서 위로 올림
-	FVector2 mid = (start + target) * 0.5f;
+	FVector2 mid = (start + target) * randMultiply;
 
 	// 거리 기반으로 휘는 정도 비율 계산 (예: 거리의 10%)
-	float curveRatio = 0.001f; // 이걸 낮추면 덜 휨
+	float randCurveMultiply = FRandom::GetRandomInRange(0.00005f, 0.00007f);
+	float curveRatio = randCurveMultiply; // 이걸 낮추면 덜 휨
 	float curveAmount = distance * curveRatio;
 
 	// 좌우 랜덤 휘기
@@ -36,6 +41,10 @@ void BulletManager::FireBullet(const FVector2& start, const FVector2& target)
 	bullet->P0 = start;
 	bullet->P1 = mid + offset;
 	bullet->P2 = target;
+
+	bullet->startPos = start;
+	bullet->currentPos = start;
+	bullet->moveDir = dirNormal;
 
 	bullets.push_back(bullet);
 }
