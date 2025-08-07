6ddbc2f8afd97c1c6fc54b5681c503810d559c86
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-08-08 00:42:31 +0900

    [fix] Prism Update add timing

--- D2D-AliceEngine/Engine/Effect/Prism.cpp
+++ D2D-AliceEngine/Engine/Effect/Prism.cpp
@@ -12,6 +12,7 @@
 Prism::~Prism()
 {
 	ClearObjects();
+	TimerManager::GetInstance().ClearTimer(timer); // 타이머 제거
 }
 
 void Prism::Initialize()
@@ -25,12 +26,26 @@ void Prism::Initialize()
 	}
 
 	// 다음 프레임의 첫 시작시에 바로 생성시켜서 트랜스폼의 지연을 막기
-	REGISTER_UPDATE_TASK_IN_SCRIPT(Update, Define::ETickingGroup::TG_PrePhysics);
+	//REGISTER_UPDATE_TASK_IN_SCRIPT(Update, Define::ETickingGroup::TG_PrePhysics);
+	TimerManager::GetInstance().ClearTimer(timer); // 이전 타이머 제거
+	TimerManager::GetInstance().SetTimer(
+		timer,
+		[this]() 
+		{ 
+		REGISTER_UPDATE_TASK_IN_SCRIPT(Update, Define::ETickingGroup::TG_PrePhysics);
+		bReady = true; 
+		},
+		0,
+		false,
+		0.1f
+		);
 }
 
 void Prism::Update(const float& deltaSeconds)
 {
 	__super::Update(deltaSeconds);
+
+	if (!bReady) return;
 
 	if (!isActive)
 		return;
