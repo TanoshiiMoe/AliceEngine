22cd219915708c22f43b1a5a5ffe79c992b24963
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-08-08 16:05:42 +0900

    [feat] Enemy Drone Spwan

--- D2D-AliceEngine/D2D-AliceEngine/Scripts/Weapon/Drone.h
+++ D2D-AliceEngine/D2D-AliceEngine/Scripts/Weapon/Drone.h
@@ -2,15 +2,38 @@
 #include <Component/ScriptComponent.h>
 #include <Manager/TimerManager.h>
 
+struct FDroneSpritePath
+{
+	std::wstring body;
+	std::wstring arm;
+
+	FDroneSpritePath() : body(L""), arm(L"") {}
+	FDroneSpritePath(std::wstring _body, std::wstring _arm) : body(_body), arm(_arm) {}
+	void operator= (const FDroneSpritePath& other)
+	{
+		body = other.body;
+		arm = other.arm;
+	}
+};
+
+enum class EDroneType
+{
+	Player,
+	Enemy
+};
 class gameObject;
 class SpriteRenderer;
 class Drone : public ScriptComponent
 {
 public:
+	Drone(FDroneSpritePath path);
+public:
 	void Initialize() override;
 
 	void FixedUpdate(const float& deltaSeconds);
 	void Update(const float& deltaSeconds) override;
+	void Floating(const float& deltaSeconds, const FVector2& dirNormal);
+	void MouseButton(const FVector2& bodyPos, const FVector2& worldMousePos, const FVector2& dirNormal);
 	void LateUpdate(const float& deltaSeconds);
 
 	virtual void Awake() override;
@@ -27,6 +50,12 @@ public:
 
 	void Input();
 
+	// Getter
+	EDroneType GetDroneType() const{ return droneType;}
+
+	// Setter
+	void SetDroneType(EDroneType type){ droneType = type;}
+
 	gameObject* m_player = nullptr;
 	gameObject* m_owner = nullptr;
 
@@ -34,6 +63,7 @@ public:
 	SpriteRenderer* arm = nullptr;
 
 	FVector2 initBodyPos = FVector2(-50.0f, 40.0f);
+	FVector2 initBodySize = FVector2(0.7f, 0.7f);
 
 	FTimerHandle timer;
 	bool bCanFire = true;
@@ -44,4 +74,7 @@ public:
 	float startY = 0.0f;
 	float endY = 7.0f;    // 위로 5만큼 이동
 	bool goingUp = true;
+
+	FDroneSpritePath spritePath;
+	EDroneType droneType = EDroneType::Enemy;
 };
