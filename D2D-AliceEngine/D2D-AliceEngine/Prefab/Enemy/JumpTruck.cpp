cdfd2a50a5c22d92fadd048f07dff68ea4fd7b22
Foodie_Pudding <sacar1920@gmail.com>
2025-08-11 21:02:16 +0900

    [fix] 점프트럭 레이어 적용
    점프트럭한테 애니메이터 컴포넌트가 존재했기때문에 적용이 되지않았던것임

--- D2D-AliceEngine/D2D-AliceEngine/Prefab/Enemy/JumpTruck.cpp
+++ D2D-AliceEngine/D2D-AliceEngine/Prefab/Enemy/JumpTruck.cpp
@@ -3,6 +3,7 @@
 #include "Component/SpriteRenderer.h"
 #include "Object/gameObject.h"
 #include "Component/SkewTransform.h"
+#include <Animation/AnimatorInstance.h>
 
 void JumpTruck::Initialize()
 {
@@ -13,13 +14,42 @@ void JumpTruck::Initialize()
 
 void JumpTruck::OnStart()
 {
+	//__super::OnStart();
+
+	//FVector2 scale = { 0.7f, 0.7f };
+
+	//if (auto anim = owner->GetComponent<AnimatorInstance>()) {
+	//	AnimatorController::LoadAnimatorController(L"Enemy/Tico/tico_sprite_AnimController.json", animController);
+	//	anim->SetAnimatorController(&animController);
+	//	anim->LoadSpriteSheet(L"Enemy/Tico/tico_sprites.json");
+	//	anim->LoadAnimationClip(L"Enemy/Tico/tico_idle_anim.json");
+	//	anim->ApplyClipDurationsToStates();
+
+	//	anim->Play();
+	//	anim->OnStart();
+	//	anim->m_layer = 19999;
+	//	// 스케일 조정
+	//	anim->SetRelativeScale(scale);
+
+	//	float height = anim->GetSpriteSizeY() * scale.y;
+	//	float yOffset = height * 0.25f;
+
+	//	SkewTransform* st = owner->GetComponent<SkewTransform>();
+	//	st->zPos += yOffset / std::sin(Math::Deg2Rad(45.0f));
+	//	//owner->transform()->SetWorldPosition(pos);
+	//}
+	//else {
+	//	owner->GetComponent<SpriteRenderer>()->LoadData(L"Enemy/Tico/tico.png");
+	//}
+
 	__super::OnStart();
+
+	owner->RemoveComponent<AnimatorInstance>(owner->GetComponent<AnimatorInstance>());
 
 	auto sr = owner->GetComponent<SpriteRenderer>();
 
 	sr->LoadData(L"Enemy/Jumporter/jumporter.png");
 	//owner->transform()->SetScale(0.35f, 0.35f);
-	sr->m_layer = 19999;
 	// 스케일 조정
 	FVector2 scale = { 0.35f, 0.35f };
 	sr->SetRelativeScale(scale);
