7628708d7e67500188ad2dd81be64e97abf1a334
Foodie_Pudding <sacar1920@gmail.com>
2025-08-14 01:22:33 +0900

    [fix] 바이크 속도표시 변경

--- D2D-AliceEngine/D2D-AliceEngine/Scripts/Widget/StageWidgetScript.cpp
+++ D2D-AliceEngine/D2D-AliceEngine/Scripts/Widget/StageWidgetScript.cpp
@@ -77,7 +77,7 @@ void StageWidgetScript::Update(const float& deltaSeconds)
 	if (WeakObjectPtr<gameObject> player = GetWorld()->FindObjectByTag<gameObject>(L"Player"))
 	{
 		if (BikeMovementScript* pm = player->GetComponent<BikeMovementScript>())
-			playerSpeed = pm->GetMaxSpeed() * pm->GetSpeedModifierValue();
+			playerSpeed = pm->GetCurrentSpeed();// * pm->GetSpeedModifierValue();
 	}
 
 	float progress = playerSpeed / 600.0f;
